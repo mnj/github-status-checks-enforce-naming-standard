1721e8944ee58ef70488c29aba3cd49206e635aa
Michael N. Jensen <michael.jensen@dustin.dk>
2021-08-30 12:35:31 +0200

    add function to get list of changed files

--- main.py
+++ main.py
@@ -56,10 +56,94 @@ def get_pull_request_json():
     except:
         raise SystemExit('Unable to parse the pull request!')
 
+def get_paginated_data(url):
+    data = []
+
+    auth_headers = {"Authorization": f"token {github_token}"}
+    try:
+        session = requests.Session()
+        retries = Retry(total=10, backoff_factor=1, status_forcelist=[ 404, 429, 500, 502, 503, 504 ])
+        session.mount('https://', HTTPAdapter(max_retries=retries))
+
+        resp = session.get(url, headers=auth_headers)
+        if resp.status_code == 200:
+            data += resp.json()
+
+            # Check if the request was paginated (it will contain a Link header)
+            # python requests already parses this header under the attribute links
+            if resp.links:
+                if "next" in resp.links:
+                    next_page_url = resp.links['next']['url']
+                    while True:
+                        resp_page = session.get(next_page_url, headers=auth_headers)
+                        if resp_page.status_code == 200:
+                            data += resp_page.json()
+
+                            # Check if there is a new next page url
+                            if resp_page.links:
+                                if "next" in resp_page.links:
+                                    next_page_url = resp_page.links['next']['url']
+                                else:
+                                    # There was no next page
+                                    break
+                        else:
+                            # We didn't get a successful response, bail out of the infinite loop
+                            # TBD: if we should just throw an exception in this case instead
+                            # raise SystemExit(f"Recieved unexpected response code: {resp.status_code} from the Github REST API!")
+                            break
+        else:
+            raise SystemExit(f"Recieved unexpected response code: {resp.status_code} from the Github REST API!")
+    except:
+        raise SystemExit(f"Could not talk to the Github API, gave up after 10 retries! {sys.exc_info()}")
+
+    return data
+
+def check_files_and_folders(pull_request_url):
+    valid = True
+    # To get the changed files, append "/files" to the pull request url, there doesn't seem to be
+    # any references to it from the pull request event data, the list of files changes, could likely
+    # be paginated, so make sure to check for that
+    
+    if "url" in pull_request_url:
+        data = get_paginated_data(f"{pull_request_url['url']}/files")
+       
+        for file_changed in data:
+            # We only care about files being added or modified
+            if ("status" in file_changed) and ("filename" in file_changed):
+                if file_changed["status"].lower() == "modified" or file_changed["status"].lower() == "added":
+                    
+                    filename = file_changed["filename"]
+                    basename = os.path.basename(filename)
+                    foldername = os.path.dirname(filename)
+
+                    if require_lowercase_files:
+                        # Check if its a excluded file
+                        if filename not in excluded_files:
+                            if not filename.islower():
+                                valid = False
+                    
+                    if require_lowercase_folders:
+                        # The foldername might be empty, if it was a file in the root of the repo
+                        if len(foldername) >0:
+                            if foldername not in excluded_folders:
+                                if not foldername.islower():
+                                    valid = False
+            else:
+                raise SystemExit('Did not find the expected element: status in the JSON response')
+
+    else:
+        raise SystemExit('Did not find the expected element: url in the pull request event data')
+    
+    return valid
+    
 ##### Main #####
 if __name__ == '__main__':
-    valid_pull_request = True # Assume true, unless checks fail
 
     pull_request_json = get_pull_request_json()
+
+    if check_files_and_folders(pull_request_json):
+        sys.exit(0)
+    else:
+        raise SystemExit('You need to fix the case of your files/folders to match the naming standard')
 
 
